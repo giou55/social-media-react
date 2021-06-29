86fb27b4e44384b1cd3ceddd3f997141ceb9a74e
Giorgos Giourmetakis <giorgosggg@gmail.com>
2021-06-29 20:57:20 +0300

    making request to api

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,14 +1,27 @@
+import { useState, useEffect } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+import axios from "axios";
 
 export default function Feed() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const res = await axios.get(
+				"posts/timeline/60d6f429f61fde4e8c6bc293"
+			);
+			setPosts(res.data);
+		};
+		fetchPosts();
+	}, []);
+	
 	return (
 		<div className="feed">
 			<div className="feedWrapper">
 				<Share />
-				{Posts.map((p) => (
+				{posts.map((p) => (
 					<Post key={p.id} post={p} />
 				))}
 			</div>
